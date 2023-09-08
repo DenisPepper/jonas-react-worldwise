a50ad150864ff23fcf6d6deaf2f0d552dcfe3176
DenisPepper <thech@yandex.ru>
2023-09-08 20:35:43 +0300

    Импортирует общие стили

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,9 +1,10 @@
 import { NavLink } from 'react-router-dom';
+import styles from './navigation.module.css';
 
 export const Navigation = () => {
   return (
-    <nav>
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.nav_list}>
         <li>
           <NavLink to='/'>Home page</NavLink>
         </li>
