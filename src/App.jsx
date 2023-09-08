fcf895842c6506561331f369e1f156e225810b72
DenisPepper <thech@yandex.ru>
2023-09-08 21:29:08 +0300

    Добавит ранее готовые файлы

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import { HomePage } from './pages/home-page';
-import { ProductPage } from './pages/product-page';
-import { PricePage } from './pages/price-page';
-import { NotFoundPage } from './pages/not-found-page';
+import Homepage from './pages/Homepage';
+import Product from './pages/Product';
+import PageNotFound from './pages/PageNotFound';
+import Pricing from './pages/Pricing';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='product' element={<ProductPage />} />
-        <Route path='price' element={<PricePage />} />
-        <Route path='*' element={<NotFoundPage />} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='price' element={<Pricing />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/product-page.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigation } from '../components/navigation';
-
-export const ProductPage = () => {
-  return <div><Navigation />
-  <h1>Product page</h1></div>;
-};
